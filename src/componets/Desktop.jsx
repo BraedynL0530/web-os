7017b61c94e7a9e98dfc8e77306c011602d62299
Braedyn L <poohdie10@gmail.com>
2026-06-12 00:51:11 -0400

    Implement Desktop component structure with basic app-launching functionality

---
+++ src/componets/Desktop.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import Background from './Background';
+import TopBar from './TopBar';
+import WindowManager from './WindowManager';
+import AppLauncher from './AppLauncher';
+import '../css/Desktop.css';
+
+function Desktop() {
+
+    const apps = useState([
+        {id:"filler" , name:"filler" , icon:"filler"}
+    ]);
+
+    const lunchApp = (app) => {// dunno how this will work yet
+        console.log(`Launching ${app.name}`);
+    }
+
+    return (
+        <div className="desktop">
+            <Background />
+            <TopBar />
+            <WindowManager />
+            <AppLauncher apps={apps} />
+        </div>
+    );
+}
+
