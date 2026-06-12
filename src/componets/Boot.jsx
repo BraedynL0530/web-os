c38111ff92c36ace6a6cc1abfe4b8cc0ddda9192
Braedyn L <poohdie10@gmail.com>
2026-06-12 00:46:18 -0400

    Add boot sequence animation with dynamic text rendering and key press handling

--- src/componets/Boot.jsx
+++ src/componets/Boot.jsx
@@ -1,27 +1,78 @@
 import '../css/Boot.css'
+import {useEffect, useState} from "react";
 
 function Boot() {
 
+    const bootLines = [
+        '[NebulaOS OS v0.1] BOOTING...',
+        'Loading kernel...',
+        'Loading initrd...',
+        'Mounting Files...',
+        'Starting System...',
+        'Welcome Back to NebulaOS :3'
+    ]
+
+    const loadFrames = [
+        '[░░░░░░░░░░░░░░░░░░░░]',
+        '[█░░░░░░░░░░░░░░░░░░░]',
+        '[██░░░░░░░░░░░░░░░░░░]',
+        '[███░░░░░░░░░░░░░░░░░]',
+        '[████░░░░░░░░░░░░░░░░]',
+        '[█████░░░░░░░░░░░░░░░]',
+        '[██████░░░░░░░░░░░░░░]',
+        '[█████████░░░░░░░░░░░]',
+        '[██████████░░░░░░░░░░]',
+        '[████████████░░░░░░░░]',
+        '[█████████████░░░░░░░]',
+        '[█████████████████░░░]',
+        '[█████████████████░░░]',
+        '[██████████████████░░]',
+        '[████████████████████]',
+
+    ]
+
+    const [currentLine, setCurrentLine] = useState(0);
+
+    useEffect(() => {
+        if (currentLine < bootLines.length) {
+            const timer = setTimeout(() => {
+                setCurrentLine(prev => prev + 1);
+            }, 400)
+            return () => clearTimeout(timer);
+        }
+    })
+
+    useEffect(() => {
+        const handleKeyDown = (event) => {
+            window.location.href = '/'; //temp location
+        };
+        window.addEventListener('keydown', handleKeyDown);
+        
+        return () => window.removeEventListener('keydown', handleKeyDown)
+    })
     return (
         <>
             <div className="boot">
                 <div className="boot-header">
                     <h1>sys boot</h1>
                     <div className="boot-text">
-                        <p className="line">[NebulaOS OS v0.1] BOOTING...</p>
-                        <p className="line">Loading kernel...</p>
-                        <p className="line">Loading initrd...</p>
-                        <p className="line">Mounting Files...</p>
-                        <p className="line">Starting System...</p>
-                        <p className="line">Welcome to NebulaOS</p>
+                        {bootLines.map((line, index) => (
+                            <p key={index} className={`line ${index < currentLine ? 'visible' : ''}`}>
+                                {line}
+                                </p>
+                        ))}
                         <p className="hint">Press any key to continue...</p>
                     </div>
                 </div>
 
                 <div className="boot-loader">
-                    
+                    {loadFrames.map((frame, index) => (
+                        <p key={index} className={`frame ${index < currentLine ? 'visible' : ''}`}></p>
+                    ))}
                 </div>
             </div>
         </>
     )
 }
+
+export default Boot
